234475f696488975424dd60a351d86c8283157c5
Sander F. Sandøy <sanderfsandoy@hotmail.com>
2021-07-11 15:54:02 +0200

    Remove toggle for rootsvandrer

--- src/features/intro/IntroPage.tsx
+++ src/features/intro/IntroPage.tsx
@@ -6,9 +6,6 @@ import React, { useState } from "react";
 import Modal from "react-modal";
 
 import BeerIcon from "./BeerIncon";
-import { OPEN_DATE } from "../../routes/FeatureRoute";
-
-const FEATURE_TOGGLE_GAMES_OPEN = new Date().getTime() > OPEN_DATE.getTime();
 
 const modalStyles = {
   content: {
@@ -61,30 +58,26 @@ const IntroPage: React.FC = () => {
           alkoholtoleranse, tenningsnivå og ikke minst koordinasjonen etter X
           antall enheter!
         </Text>
-        {FEATURE_TOGGLE_GAMES_OPEN && (
-          <>
-            <Text>
-              Etter endt konkurranse så vil vinneren kunne smykke seg med evig
-              ære, samt at man tar med seg&nbsp;
-              <span
-                style={{ textDecoration: "underline" }}
-                onClick={() => setModalOpen(true)}
-              >
-                Rootsvandrer`n
-              </span>
-              &nbsp;hjem til odel og eie - frem til neste års leker!
-            </Text>
-            <Text>Let the games begin!</Text>
-            <Modal
-              isOpen={modalOpen}
-              onRequestClose={closeModal}
-              style={modalStyles}
-            >
-              <Image src={rootsVandrern} />
-              <p style={{ color: "black" }}>Rootsvandrer`n</p>
-            </Modal>
-          </>
-        )}
+        <Text>
+          Etter endt konkurranse så vil vinneren kunne smykke seg med evig ære,
+          samt at man tar med seg&nbsp;
+          <span
+            style={{ textDecoration: "underline" }}
+            onClick={() => setModalOpen(true)}
+          >
+            Rootsvandrer`n
+          </span>
+          &nbsp;hjem til odel og eie - frem til neste års leker!
+        </Text>
+        <Text>Lykke til!</Text>
+        <Modal
+          isOpen={modalOpen}
+          onRequestClose={closeModal}
+          style={modalStyles}
+        >
+          <Image src={rootsVandrern} />
+          <p style={{ color: "black" }}>Rootsvandrer`n</p>
+        </Modal>
       </Container>
     </Page>
   );
